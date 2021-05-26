c4b722bbd3c0c7ec6fb68581f421e5a054bdd2f5
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2021-05-26 10:45:01 +0900

    FastlaneAPP add

---
+++ FastlaneAPP/FastlaneAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastlaneAPP
+//
+//  Created by Swift-Beginners on 2021/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastlaneAPP/FastlaneAPP/FastlaneAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneAPPApp.swift
+//  FastlaneAPP
+//
+//  Created by Swift-Beginners on 2021/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

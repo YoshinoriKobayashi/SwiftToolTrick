3f05257433baa3108602f87167beaad1c868f055
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2021-06-07 09:24:54 +0900

    お菓子アプリのカスタマイズ

--- MyOkashiCustom/MyOkashiCustom/ContentView.swift
+++ MyOkashiCustom/MyOkashiCustom/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    // OkashiDataを参照する状態変数
+    // 複数データを外部と共有する
+    // ObservedObject
+    @ObservedObject var okashiDataList = OkashiData()
+    // 入力された文字列を保持する状態変数
+    @State var inputText = ""
+
     var body: some View {
-        Text("Hello, world!")
+        // 垂直にレイアウト（縦方向にレイアウト）
+        VStack {
+            // 文字を受け取るTextFieldを表示する
+            TextField("キーワードを入力してください", text: $inputText,onCommit: {
+                // 入力完了直後に検索をする
+                okashiDataList.searchOkashi(keyword: inputText)
+            })
             .padding()
+            // リスト表示する
+            List(okashiDataList.okashiList) { okashi in
+                // okashiに要素を取り出して、List（一覧）を生成する
+                // 水平にレイアウト（横方向にレイアウト）
+                HStack {
+                    // 画像を表示する
+                    Image(uiImage: okashi.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height:40)
+                    // テキスト表示する
+                    Text(okashi.name)
+                }
+            }
+        }
     }
 }
 
--- MyOkashiCustom/MyOkashiCustom/SafariView.swift
+++ MyOkashiCustom/MyOkashiCustom/SafariView.swift
@@ -6,15 +6,20 @@
 //
 
 import SwiftUI
+import SafariServices
 
-struct SafariView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// SFSafariViewControllerを起動する構造体
+struct SafariView: UIViewControllerRepresentable {
+    // 表示するホームページのURLを受ける変数
+    var url:URL
+    // 表示するViewを生成するときに実行
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        // Safariを起動
+        return SFSafariViewController(url: url)
+    }
+    // Viewが更新されたときに実行
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
+        //　処理なし
     }
 }
 
-struct SafariView_Previews: PreviewProvider {
-    static var previews: some View {
-        SafariView()
-    }
-}

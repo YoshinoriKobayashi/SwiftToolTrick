a7d727332b15e525ffb995e0b6d0fd8502df1775
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2021-06-09 09:44:49 +0900

    FastlaneApp,MyOkashiCustomをアップデート

--- MyOkashiCustom/MyOkashiCustom/ContentView.swift
+++ MyOkashiCustom/MyOkashiCustom/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
     @ObservedObject var okashiDataList = OkashiData()
     // 入力された文字列を保持する状態変数
     @State var inputText = ""
+    // SafariViewの表示有無を管理する変数
+    @State var showSafari = false
 
     var body: some View {
         // 垂直にレイアウト（縦方向にレイアウト）
@@ -27,17 +29,30 @@ struct ContentView: View {
             .padding()
             // リスト表示する
             List(okashiDataList.okashiList) { okashi in
-                // okashiに要素を取り出して、List（一覧）を生成する
-                // 水平にレイアウト（横方向にレイアウト）
-                HStack {
-                    // 画像を表示する
-                    Image(uiImage: okashi.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height:40)
-                    // テキスト表示する
-                    Text(okashi.name)
-                }
+                // １つ１つ要素が取り出される
+                // ボタンを用意する
+                Button(action: {
+                    // SafariViewを表示する
+                    showSafari.toggle()
+                }) {
+                    // okashiに要素を取り出して、List（一覧）を生成する
+                    // 水平にレイアウト（横方向にレイアウト）
+                    HStack {
+                        // 画像を表示する
+                        Image(uiImage: okashi.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height:40)
+                        // テキスト表示する
+                        Text(okashi.name)
+                    }
+                }//
+                .sheet(isPresented: self.$showSafari, content: {
+                    // SafariViewを表示する
+                    SafariView(url: okashi.link)
+                        // 画面下部をいっぱになるようにセーフエリア外までいっぱいになるように指定
+                        .edgesIgnoringSafeArea(.bottom)
+                })
             }
         }
     }
--- MyOkashiCustom/MyOkashiCustom/SafariView.swift
+++ MyOkashiCustom/MyOkashiCustom/SafariView.swift
@@ -2,7 +2,7 @@
 //  SafariView.swift
 //  MyOkashiCustom
 //
-//  Created by Swift-Beginners on 2021/06/07.
+//  Created by Swift-Beginners on 2021/06/09.
 //
 
 import SwiftUI
@@ -11,15 +11,16 @@ import SafariServices
 // SFSafariViewControllerを起動する構造体
 struct SafariView: UIViewControllerRepresentable {
     // 表示するホームページのURLを受ける変数
-    var url:URL
+    var url: URL
+
     // 表示するViewを生成するときに実行
-    func makeUIViewController(context: Context) -> SFSafariViewController {
+    func makeUIViewController(context: Context) -> some UIViewController {
         // Safariを起動
         return SFSafariViewController(url: url)
     }
+
     // Viewが更新されたときに実行
-    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
-        //　処理なし
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        // 処理なり
     }
 }
-

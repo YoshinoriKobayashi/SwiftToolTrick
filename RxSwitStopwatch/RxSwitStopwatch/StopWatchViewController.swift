2b749f5d972801c1c4970fc441bab999078ed984
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2022-02-18 17:04:59 +0900

    swiftlintの削除、modelの追加

--- RxSwitStopwatch/RxSwitStopwatch/StopWatchViewController.swift
+++ RxSwitStopwatch/RxSwitStopwatch/StopWatchViewController.swift
@@ -38,13 +38,21 @@ class ViewController: UIViewController {
     private var viewModel = StopWatchViewModelType
     private var disposeBag = DisposeBag()
     
-    init(viewModel: )
+    init(viewModel: StopWatchViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupSubView()
+        bind()
+        viewModel.inputs.ioPaueTimer.accept(false)
     }
-
-
 }
 
--- RxSwitStopwatch/RxSwitStopwatch/StopWatchViewModel.swift
+++ RxSwitStopwatch/RxSwitStopwatch/StopWatchViewModel.swift
@@ -15,5 +15,57 @@ protocol StopWatchViewModelInputs: AnyObject {
 }
 
 protocol StopWatchViewModelOutputs: AnyObject {
-    var isTimerWorked: Driver<bool>
+    var isTimerWorked: Driver<Bool> { get }
+    var timerText: Driver<String> { get }
+    var isResetButtonHidden: Driver<bool> { get }
+}
+
+protocol StopWatchViewModelType: AnyObject {
+    var iputs: StopWatchViewModelInputs { get }
+    var outputs: StopWatchViewModelOutputs { get }
+}
+
+final class StopWatchViewModel: StopWatchViewModelType. StopWatchViewModelInputs,StopWatchViewModelOutputs {
+    var inputs: StopWatchViewModelInputs { return self }
+    var putputs: StopWatchViewModelOutputs{ return self }
+    
+    // MARK: - Input
+    let isPauseTimer = PublishRelay<Bool>()
+    var isResetButtonTaped = PublishRelay<Void>()
+    
+    // MARK: - Output
+    let isTimerWorked: Driver<Bool>
+    let timerText: Driver<String>
+    let isResetButtonHidden: Driver<Bool>
+    
+    private let disposeBag = DisposeBag()
+    private let totalTimerDuration = BehaviorRelay<Int>(value: 0)
+    
+    init() {
+        isTimerWorked = isPauseTimer.asDriver(onErrorDriveWith: .empty())
+        
+        timerText = totalTimerDuration
+            .map { String("\(Double($0) / 10)")}
+            .asDriver(onErrorDriveWith: .empty())
+        
+        isResetButtonHidden = Observable.merge(isTimerWorked.asObservable(), isResetButtonTaped.map { _ in true }.asObservable())
+            .skip(1)
+            .asDriver(onErrorDriveWith: .empty())
+        
+        isTimerWorked.asObservable()
+            .flatMapLatest { [weak self] isWorked -> Observable<Int> in 
+                if isWorked {
+                    return Observable<Int>.interval(0.1, scheduler: MainScheduler.instance)
+                        .withLatestFrom(Observable<Int>.just(self?.totalTimerDuration.value ?? 0)) { ($0 + $1)}
+                } else {
+                    return Observable<Int>.just(self?.totalTimerDuration.value ?? 0)
+                }
+            } 
+            .bind(to: totalTimerDuration)
+            .disposed(by: disposeBag)
+        
+        isResetButtonTaped.map { _ in 0 }
+            .bind(to: totalTimerDuration)
+            .disposed(by: disposeBag)
+    }
 }

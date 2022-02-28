c101dc6051605d3bb8ea3dc362430caf1d480fd4
YoshinoriKobayashi <yoshiiikoba@gmail.com>
2022-02-28 19:26:28 +0900

    RxDataSources_template add

---
+++ RxDataSources_template/README.md
@@ -0,0 +1,5 @@
+# RxDataSourceを使ってUITableViewのデータバインディング
+
+
+### 参考ページ
+[RxDataSourceを使ってUITableViewのデータバインディングを実装するテンプレートを作ってみた](https://tamappe.com/2019/03/07/2019-03-07-200000/)
---
+++ RxDataSources_template/RxDataSources_template/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  RxDataSources_template
+//
+//  Created by kobayashi on 2022/02/28.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+
+/// TableViewCellに紐付けるDataModel
+struct CustomCellModel {
+    var name: String
+    var email: String
+}
+
+/// セクションヘッダーの名前とセクション内のitem
+struct SectionOfCustomData {
+    /// セクションヘッダーの名前
+    var header: String
+    /// indexPath.row のcellデータ
+    var items: [Item]
+}
+
+/// RxDataSourceを使ってDataModelとdataSourceを紐付けるため
+extension SectionOfCustomData: SectionModelType {
+    // ItemにCellのDataModelに紐付ける
+    typealias Item = CustomCellModel
+    
+    // ほぼテンプレで可能
+    init(original: SectionOfCustomData,items: [Item]) {
+        self = original
+        self.items = items
+    }
+    
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

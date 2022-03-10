d943af7156f4ca9bd8e99729139a9e78db3661b3
YoshinoriKobayashi <yoshiiikoba@gmail.com>
2022-03-11 06:52:38 +0900

    sampleCollection add

---
+++ Playground/sampleCollection.playground/Pages/truncatingRemainder.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let result = 4.00
+
+print(result.truncatingRemainder(dividingBy: 1))
+
+// BASE関数は、この値を指定された値で割った余りを、切り捨て計算で返します。
+// dividingBy:この値を除算する際に使用する値。
+// retun:この値を切り捨て除算で他で割った余り。
+
+print(0.0 == 0)
+

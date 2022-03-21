3b399f7537019a5f0a1553b626fb30b6169f8134
YoshinoriKobayashi <yoshiiikoba@gmail.com>
2022-03-22 05:45:58 +0900

    RxSwift_playground add

---
+++ RxSwiftKenkyuDokuhon/RxSwiftKenkyuDokuhon/RxSwiftKenkyuDokuhonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwiftKenkyuDokuhonApp.swift
+//  RxSwiftKenkyuDokuhon
+//
+//  Created by kobayashi on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct RxSwiftKenkyuDokuhonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RxSwift_Playground/RxSwift_Playground/RxSwift_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwift_PlaygroundApp.swift
+//  RxSwift_Playground
+//
+//  Created by kobayashi on 2022/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RxSwift_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

35a3448728cd4a9db28471800e34aed036c010dd
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2022-05-30 14:03:55 +0900

    画面遷移のときのパラメタ渡し

--- UIKitCustomView/UIKitCustomView/SampleView.swift
+++ UIKitCustomView/UIKitCustomView/SampleView.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 final class SampleView: UIView {
-    init() {
+
+    @IBOutlet weak var AmazonButton: UIButton!
+    @IBOutlet weak var YahooButton: UIButton!
+    @IBOutlet weak var RukkutenButton: UIButton!
+
+    var productName = ""
+    var categoryName = ""
+
+    // UIViewの場合は、initで引数をもらうと画面のコントールに設定できる
+    init(productName:String,categoryName:String) {
         super.init(frame: .zero)
 
         // Xibを使用するためのコード
@@ -16,9 +25,65 @@ final class SampleView: UIView {
         view.frame = bounds
         addSubview(view)
         view.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+
+        print(productName)
+        print(categoryName)
+
+        AmazonButton.setTitleColor(UIColor.red, for: .normal)
+        AmazonButton.layer.borderColor = UIColor.red.cgColor
+        AmazonButton.layer.borderWidth = 2
+
+//
+//        YahooButton.layer.borderColor = Color.red
+//        RukkutenButton.layer.borderColor = Color.red
+
     }
 
+    // 必ず必要
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been im")
+        fatalError("init(coder:) has not been implemented")
     }
+
+
+    // 制約の更新に関するメソッド
+    // 開発者が直接呼び出すのはNG→呼び出すと落ちる
+//    override func updateConstraints() {
+//        print("== updateConstraints")
+//    }
+    // - 制約更新の実行要否のフラグを立てる 
+    // - 計算実行タイミングはシステム任せ
+    override func setNeedsUpdateConstraints() {
+        print("== setNeedsUpdateConstraints")
+    }
+    // 制約更新を即座に実行（更新フラグあれば）
+    override func updateConstraintsIfNeeded() {
+        print("== updateConstraintsIfNeeded")
+    }
+
+    // レイアウトに関するメソッド
+    // - frameの更新を実行 
+    // - 開発者が直接呼び出すのはNG
+    override func layoutSubviews() {
+        print("== layoutSubviews")
+    }
+    // - frame更新要否のフラグを立てる 
+    // - 計算実行タイミングはシステム任せ
+    override func setNeedsLayout() {
+        print("== setNeedsLayout")
+    }
+    // frame更新を即座に実行（更新フラグあれば）
+    override func layoutIfNeeded() {
+        print("== layoutIfNeeded")
+    }
+
+    // 描画に関するメソッド
+    func draw() {
+        print("== draw")
+    }
+    // - 描画更新の実行要否のフラグを立てる
+    // 制約やレイアウト更新のように、 即時実行用のメソッドはなし
+    override func setNeedsDisplay() {
+        print("== setNeedsDisplay")
+    }
+
 }
--- UIKitCustomView/UIKitCustomView/ViewController.swift
+++ UIKitCustomView/UIKitCustomView/ViewController.swift
@@ -11,17 +11,26 @@ import SnapKit
 class ViewController: UIViewController {
 
     // カスタムViewの使用
-    private let sampleView = SampleView()
+    private let sampleView = SampleView(productName: "明治チョコボール", categoryName: "チョコレート")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // 遷移先にプロパティ（変数）を設定する場合
+//        sampleView.productName = "明治チョコボール"
+//        sampleView.categoryName = "チョコレート"
+
+        // 遷移先のラベルを設定する場合はこれでいい
+//        sampleView.productNameLabel.text = "明治チョコボール"
+//        sampleView.categoryNameLabel.text = "チョコレート"
 
         // Viewを表示する
         view.addSubview(sampleView)
         // snpでAutoLayoutを設定
         sampleView.snp.makeConstraints { make in
             make.center.equalToSuperview()
-            make.width.height.equalTo(200)
+            make.height.equalTo(200)
+            make.width.equalToSuperview()
         }
     }
 

da23b647bd3fde690240a098900ad925761a1f76
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2022-09-05 17:28:10 +0900

    SwiftUIでのNavigationの背景とタイトル、アイテムの色変更

--- SwiftUINavigationView/SwiftUINavigationView/NavigationViewToolBar.swift
+++ SwiftUINavigationView/SwiftUINavigationView/NavigationViewToolBar.swift
@@ -8,6 +8,35 @@
 import SwiftUI
 
 struct NavigationViewToolBar: View {
+    
+    init() {
+        let backgroundColor = UIColor(red: 73/255, green: 119/255, blue: 172/255, alpha: 1.0)
+        // ナビゲーションバーはSwiftUIの配色は実現できない
+        if #available(iOS 15.0, *) {
+            let appearance = UINavigationBarAppearance()     
+            //ナビゲーションバーの背景を変更
+            appearance.backgroundColor = backgroundColor
+            //ナビゲーションのタイトル文字列の色を変更
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+            // ナビゲーションバーに反映
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+            //ナビゲーションアイテムの色を変更
+            UINavigationBar.appearance().tintColor = UIColor.white
+
+
+        } else {
+            // iOS14以前
+            //ナビゲーションバーの背景を変更
+            UINavigationBar.appearance().barTintColor = backgroundColor
+            //ナビゲーションのタイトル文字列の色を変更
+            UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+            //ナビゲーションアイテムの色を変更→accentColorで設定できるので不要
+            // UINavigationBar.appearance().tintColor = UIColor.white
+                    
+        }
+
+    }
     var body: some View {
         NavigationView {
             List(1..<20) { index in
@@ -16,7 +45,7 @@ struct NavigationViewToolBar: View {
                         Text("\(index)行目")
                 }
             }
-            .navigationTitle("NavigationView")
+            .navigationTitle("ナビゲーションタイトル")
             .navigationBarTitleDisplayMode(.inline)
 
             .toolbar {
@@ -53,6 +82,8 @@ struct NavigationViewToolBar: View {
                 }
             }
         }
+        .accentColor(.white) // iOS14から対応している。
+        // https://stackoverflow.com/questions/64467504/how-to-change-color-of-toolbaritem-with-navigationbarleading-placement-in-swiftu
     }
 }
 

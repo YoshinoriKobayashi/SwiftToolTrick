6573fb8ca199fc0472e0eeb3971d7da90e5ea1d5
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2022-11-07 09:26:57 +0900

    TextKitを使ったサンプルの追加

---
+++ TextHighlight/README.md
@@ -0,0 +1,11 @@
+# テキストのハイライトで背景や角丸をつける
+
+- addAttributesの機能だけではできない。
+- TextKitの機能を利用する
+
+
+## 参考
+- [テキストを装飾する／NSAttributedStringの属性一覧](https://qiita.com/shu223/items/c2eb639976154a491d19)
+- [【Swift】NSAttributedString.Keyのまとめ](https://www.2nd-walker.com/2021/08/30/swift-list-nsattributedstring-keys/)
+
+
--- TextHighlight/TextHighlight/ViewController.swift
+++ TextHighlight/TextHighlight/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController : UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        // 複数行の表示
+        textLabel.numberOfLines = 0
+        textLabel.lineBreakMode = .byClipping
+        
+        textLabel.text = "Mind can be observed and known. But you can know directly only your own mind, and not another's."
+        
+        textView.text = "Mind can be observed and known. But you can know directly only your own mind, and not another's."
 
-
+//        
+//        // フォントの指定
+//        guard let originalFont = UIFont(name:"Comic Sans MS", size: 24.0) else { return }
+//        // 太字の属性を作成
+//        let boldFontDescriptor = originalFont.fontDescriptor.withSymbolicTraits(.traitBold)
+//        // 
+//        let boldFont = UIFont(descriptor: boldFontDescriptor!, size: (originalFont.pointSize))
+        
+        //
+        // let newText = NSMutableAttributedString(attributedString: textLabel.attributedText!)
+//        let newText = NSMutableAttributedString(string: textLabel.text!,attributes: [.font: originalFont])
+        let newText = NSMutableAttributedString(string: textLabel.text!)
+        //
+//        newText.addAttribute(.font, value: boldFont, range: NSRange(location: 0, length: 4))
+        
+        // フォントの指定
+        newText.addAttribute(.font, value: UIFont(name: "PartyLetPlain", size: 24)!,  range: NSRange(0...3))
+        
+        // 背景の指定
+        newText.addAttribute(.backgroundColor, value: UIColor.green,  range: NSRange(12...19))
+        
+        textLabel.attributedText = newText
+        textView.attributedText = newText
+     }
 }
 

8acdf1ff2bb0b88b2923b70e151113ce682a05c0
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2022-11-21 18:13:27 +0900

    角丸でのテキストハイライト

--- TextHighlight2/TextHighlight2/ViewController.swift
+++ TextHighlight2/TextHighlight2/ViewController.swift
@@ -8,64 +8,113 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
 class Label: UILabel {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.text = "{filter:it was} working in {ignore:hugo} {ignore:ash lee}"
-        self.font = UIFont.boldSystemFont(ofSize: 20)
+        self.text = "That's right by the way {filter:the woerd silver} has a {ignore:special meaning} in {filter:japon}. Can you {ignore:guess} what it is?"
+        self.font = UIFont.boldSystemFont(ofSize: 14)
         // UILabelはlayoutManagerがない
     }
-
+    
 }
 
 class TextView: UITextView {
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.text = "That's right by the way {filter:the woerd silver} has a {ignore:special meaning} in Japan. Can you {ignore:guess} what it is?"
+        self.text = "That's right by the way {filter:the woerd silver} has a {ignore:special meaning} in {filter:japon}. Can you {ignore:guess} what it is?"
         self.font = UIFont.boldSystemFont(ofSize: 14)
         self.textColor = UIColor.green
         let aaa = setAttribute(uiView: self)
         print(aaa)
-
+        
     }
     
 }
 
-
+// UILabelではダメ、UITextViewでないとlayoutManagerのプロパティが使えない
+// iOS15とiOS16で検証すること！！
 func setAttribute(uiView: UITextView) -> Bool{
-
+    
     // {filter:...}、{ignore:...}がヒットする正規表現
     // let pattern = "\\{.*?:.*?\\}"
-
+    
     
     // {filter:...}
     // ??? に置換
     // 背景をグレー
     //
-
+    
     var pattern = "\\{filter:.*?\\}"
     var regex = try! NSRegularExpression(pattern: pattern)
-    var replace = uiView.text.replacingOccurrences(of: pattern, with: " ??? ",  options: .regularExpression, range: nil)
-
     // 置換後の文字を設定
-    uiView.text = replace
+    uiView.text = uiView.text.replacingOccurrences(of: pattern, with: " ??? ",  options: .regularExpression, range: nil)
+        
+    // {ignore:...}
+    // イタリックにする
+    // {ignore:...}を文字に置換する、もしくは除く
+    pattern = "\\{ignore:.*?\\}"
+    regex = try! NSRegularExpression(pattern: pattern)
+    var matches = regex.matches(in: uiView.text, range: NSRange(0..<uiView.text.count))
+    
+    // 斜体にする
+    for index in matches.indices {
+        let attributeString = NSMutableAttributedString(attributedString: uiView.attributedText)
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "Avenir-BookOblique", size: 14.0)!
+        ]
+        attributeString.addAttributes(attributes,range: matches[index].range)
+        uiView.attributedText = attributeString   
+    }
+    // 置換
+    pattern = "\\{ignore:"
+    regex = try! NSRegularExpression(pattern: pattern)
+    // ヒットしなくなるまで削除を繰り返す
+    while true {
+        let firstRange = regex.rangeOfFirstMatch(in: uiView.text, range: NSRange(0..<uiView.text.count))
+        
+        // ヒットしなくなる
+        if firstRange.location == NSNotFound {
+            break
+        }
+        let attributeString = NSMutableAttributedString(attributedString: uiView.attributedText)
+        // 文字の削除
+        attributeString.deleteCharacters(in: firstRange)
+        uiView.attributedText = attributeString   
+    }
+    pattern = "\\}"
+    regex = try! NSRegularExpression(pattern: pattern)
+    // ヒットしなくなるまで削除を繰り返す
+    while true {
+        // パターンにヒットした最初の1件の範囲を取得
+        let firstRange = regex.rangeOfFirstMatch(in: uiView.text, range: NSRange(0..<uiView.text.count))
+        // ヒットしなくなる
+        if firstRange.location == NSNotFound {
+            break
+        }
+        let attributeString = NSMutableAttributedString(attributedString: uiView.attributedText)
+        // 文字の削除
+        attributeString.deleteCharacters(in: firstRange)
+        uiView.attributedText = attributeString           
+    }
+    
     
     // ??? を検索して、背景を塗る
+    // 角丸背景は文字位置からViewを描くので、文字数が確定した一番最後に処理をする。
     pattern = #"\?{3}"#
     regex = try! NSRegularExpression(pattern: pattern)
-    var matches = regex.matches(in: replace, range: NSRange(0..<replace.count))
+    matches = regex.matches(in: uiView.text, range: NSRange(0..<uiView.text.count))
     // rangeで文字列の位置を探せる
-//    let range = (uiView.text as NSString).range(of: "ignore")
+    //    let range = (uiView.text as NSString).range(of: "ignore")
     // 与えられたコンテナ内にある、与えられた glyphRange 内のグリフを完全に囲む最小の外接矩形を返します。
     // 範囲は、外接矩形を計算する前に、コンテナの範囲と交差されます。
     // このメソッドは、グリフ範囲を無効化するための表示矩形に変換するために用いることができます。 
@@ -103,45 +152,7 @@ func setAttribute(uiView: UITextView) -> Bool{
         attributeString.addAttributes(attributes,range: matches[index].range)
         uiView.attributedText = attributeString
     }
-
-    // {ignore:...}
-    // イタリックにする
-    // {ignore:...}を文字に置換する、もしくは除く
-    pattern = "\\{ignore:.*?\\}"
-    regex = try! NSRegularExpression(pattern: pattern)
-    matches = regex.matches(in: replace, range: NSRange(0..<replace.count))
     
-    // 斜体にする
-    for index in matches.indices {
-        let attributeString = NSMutableAttributedString(attributedString: uiView.attributedText)
-        let attributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont(name: "Avenir-BookOblique", size: 14.0)!
-        ]
-        attributeString.addAttributes(attributes,range: matches[index].range)
-        uiView.attributedText = attributeString   
-    }
-    // 置換
-    pattern = "\\{ignore:"
-    regex = try! NSRegularExpression(pattern: pattern)
-    while true {
-        matches = regex.matches(in: replace, range: NSRange(0..<uiView.text.count))
-        for index in matches.indices {
-            let attributeString = NSMutableAttributedString(attributedString: uiView.attributedText)
-            // 文字の削除
-            attributeString.deleteCharacters(in: matches[index].range)
-            uiView.attributedText = attributeString   
-        }        
-    }
-    pattern = "\\}"
-    regex = try! NSRegularExpression(pattern: pattern)
-    matches = regex.matches(in: replace, range: NSRange(0..<uiView.text.count))
-    for index in matches.indices {
-        let attributeString = NSMutableAttributedString(attributedString: uiView.attributedText)
-        // 文字の削除
-        attributeString.deleteCharacters(in: matches[index].range)
-        uiView.attributedText = attributeString   
-    }
-        
     
     return true
     

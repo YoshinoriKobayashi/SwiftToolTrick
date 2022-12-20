9b67db7622e62991c00faaaa13cbbd4bd6b68f7a
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2022-12-20 19:49:50 +0800

    コメントの追加

--- UIKitTextView_tap/UIKitTextView_tap/ViewController.swift
+++ UIKitTextView_tap/UIKitTextView_tap/ViewController.swift
@@ -24,6 +24,9 @@ class ViewController: UIViewController {
     
     // タップハンドラメソッドを追加
     @objc private final func tapOnTextView(_ tapGesture: UITapGestureRecognizer) {
+        
+        // ジェスチャー認識器によって表現されたジェスチャーの与えられたビューにおける位置として計算された点を返します。
+        // CGPoint:2次元座標系における点を含む構造体。
         let point = tapGesture.location(in: textView)
         if let detectedWord = getWordAtPosition(point) {
             print(detectedWord)
@@ -32,10 +35,16 @@ class ViewController: UIViewController {
     
     // ポイントから単語を得る
     private final func getWordAtPosition(_ point: CGPoint) -> String?{
-        if let textPosition = textView.closestPosition(to: point)
-        {
-            if let range = textView.tokenizer.rangeEnclosingPosition(textPosition, with: .word, inDirection: UITextDirection(rawValue: 1))
-            {
+        
+        // 指定された点に最も近い文書内の位置を返します。
+        // UITextPosition:テキストコンテナ内の位置、つまりテキスト表示ビューのバックストリングへのインデックス。
+        if let textPosition = textView.closestPosition(to: point){
+            // rangeEnclosingPosition:指定された粒度のテキスト単位で、指定された方向のテキスト位置を囲むテキストの範囲を返す。
+            // position:文書中の位置を表すtext-positionオブジェクト。
+            // with:テキスト単位のある粒度を示す定数。
+            // inDirection:位置に対する相対的な方向を示す定数。この定数は UITextStorageDirection または UITextLayoutDirection 型である。
+            if let range = textView.tokenizer.rangeEnclosingPosition(textPosition, with: .word, inDirection: UITextDirection(rawValue: 1)){
+                // ドキュメントにはないメソッド。rangeからテキストを取り出す
                 return textView.text(in: range)
             }
         }
